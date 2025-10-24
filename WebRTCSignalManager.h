6ff01feaa89cf4781915429956967101aaf86671
HopeTiga <158744351+HopeTiga@users.noreply.github.com>
2025-10-24 11:05:29 +0800

    Add files via upload
    新增lru缓存，避免每次都进行二级路由，通过缓存保证1次路由直达

---
+++ WebRTCSignalManager.h
@@ -0,0 +1,56 @@
+#pragma once
+#define TBB_PREVIEW_CONCURRENT_LRU_CACHE 1
+#include "WebRTCSignalServer.h"
+#include "WebRTCSignalSocket.h"
+#include <vector>
+
+#include <boost/asio.hpp>
+
+#include <tbb/concurrent_unordered_map.h>
+#include <tbb/concurrent_lru_cache.h>
+
+
+
+
+namespace Hope {
+
+	class WebRTCSignalServer;
+
+	class WebRTCSignalManager : public std::enable_shared_from_this<WebRTCSignalManager>
+	{
+	public:
+
+		WebRTCSignalManager(boost::asio::io_context & ioContext,int channelIndex,WebRTCSignalServer * webrtcSignalServer);
+
+		~WebRTCSignalManager();
+
+		std::shared_ptr<WebRTCSignalSocket> generateWebRTCSignalSocket();
+
+		boost::asio::io_context& getIoComplatePorts();
+
+		void handleMessage(boost::json::object message, std::shared_ptr<WebRTCSignalSocket> webrtcSignalSocket);
+
+		void removeConnection(const std::string& accountID);
+
+		tbb::concurrent_unordered_map<std::string,int>& getActorSocketMappingIndex();
+
+	private:
+
+		boost::asio::io_context & ioContext;
+
+		int channelIndex;
+
+		tbb::concurrent_unordered_map<std::string, std::shared_ptr<WebRTCSignalSocket>> webrtcSignalSocketMap;
+
+		WebRTCSignalServer* webrtcSignalServer;
+
+		size_t hashSize = std::thread::hardware_concurrency() * 2;
+
+		tbb::concurrent_unordered_map<std::string, int> actorSocketMappingIndex;
+
+		tbb::concurrent_lru_cache<std::string, int> localRouteCache ;
+
+		std::hash<std::string> hasher;
+	};
+}
+
