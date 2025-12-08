8bb2fb66f8443a70a54add52431c6ef512a0826b
HopeTiga <158744351+HopeTiga@users.noreply.github.com>
2025-12-08 14:26:01 +0800

    Add files via upload

--- MsquicStorage/main.cpp
+++ MsquicStorage/main.cpp
@@ -20,7 +20,7 @@ int main() {
 
     boost::asio::io_context ioContext;
 
-    size_t port = ConfigManager::Instance().GetInt("MsquicServer.port");
+    size_t port = ConfigManager::Instance().GetInt("MsquicStorage.port");
 
     std::unique_ptr<boost::asio::executor_work_guard<boost::asio::io_context::executor_type>> work = std::make_unique<boost::asio::executor_work_guard<boost::asio::io_context::executor_type>>(boost::asio::make_work_guard(ioContext));
 
