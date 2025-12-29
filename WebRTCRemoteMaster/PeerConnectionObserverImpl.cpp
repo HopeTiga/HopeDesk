de791488212d3abd1281e113f7347944f47aebdf
HopeTiga <158744351+HopeTiga@users.noreply.github.com>
2025-12-29 10:01:15 +0800

    Add files via upload

--- WebRTCRemoteMaster/PeerConnectionObserverImpl.cpp
+++ WebRTCRemoteMaster/PeerConnectionObserverImpl.cpp
@@ -175,6 +175,14 @@ namespace hope {
         // 空实现
     }
 
+    void PeerConnectionObserverImpl::OnIceCandidateError(const std::string& address, int port, const std::string& url,
+                                                         int errorCode, const std::string& errorText) {
+
+        printf("PeerConnectionObserverImpl::OnIceCandidateError: address=%s, port=%d, url=%s, errorCode=%d, errorText=%s\n",
+               address.c_str(), port, url.c_str(), errorCode, errorText.c_str());
+
+    }
+
 	}
 
 }
