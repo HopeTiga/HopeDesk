586fbfdd32ff541c80ee3c8f7dd4441074449487
HopeTiga <158744351+HopeTiga@users.noreply.github.com>
2026-02-04 10:55:28 +0800

    Add files via upload

--- WebRTCSystemLogon/WebRTCManager.h
+++ WebRTCSystemLogon/WebRTCManager.h
@@ -167,10 +167,10 @@ namespace hope {
             static webrtc::RtpEncodingParameters getDefaultRtpEncodingParameters() {
                 webrtc::RtpEncodingParameters encoding;
                 encoding.active = true;
-                encoding.max_bitrate_bps = 1800000;  // 4 Mbps
-                encoding.min_bitrate_bps = 1800000;  // 1 Mbps
+                encoding.max_bitrate_bps = 2000000;  // 4 Mbps
+                encoding.min_bitrate_bps = 2000000;  // 1 Mbps
                 encoding.bitrate_priority = 4.0;
-                encoding.max_framerate = 120;
+                encoding.max_framerate = 60;
                 encoding.scale_resolution_down_by = 1.0;
                 encoding.bitrate_priority = 1.0;
                 encoding.scalability_mode = "L1T1";
