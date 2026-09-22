806356aa0fbc8cdf8c9a336a085c43323230ee4c
HopeTiga <158744351+HopeTiga@users.noreply.github.com>
2026-09-22 15:11:31 +0800

    Add files via upload

--- WebrtcSignalServer/utils/CompletionHandle.h
+++ WebrtcSignalServer/utils/CompletionHandle.h
@@ -9,8 +9,6 @@ namespace hope {
 
 	struct CompletionHandle {
 
-		// 位置必须记在构造点：默认参数里的 current() 在调用点求值，报出来才是 co_spawn 那一行。
-		// 直接用 LOG_ERROR 的话，所有协程的异常都会显示成这个文件里的 catch 行。
 		explicit CompletionHandle(std::source_location location = std::source_location::current())
 			: spawnLocation(location)
 		{
@@ -38,7 +36,7 @@ namespace hope {
 			}
 			catch (...) {
 
-				LOG_ERROR_FROM(spawnLocation, "CoSpawn Exception: unknown exception");
+				LOG_ERROR_FROM(spawnLocation, "CoSpawn Exception: Unknown Exception");
 
 			}
 
